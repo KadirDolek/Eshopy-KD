c5222ab332150e560aafffe545793d0f2b79544f
kadirdolek <kadirdolek@outlook.com>
2025-05-19 14:10:02 +0200

    placement div panier

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,20 @@ import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Content from './Components/Content/Content'
 import data from './data.json'
-console.log(data);
+import Panier from './Components/Panier/Panier'
+
 
 
 function App() {
-
-
   return (
     <>
       <Navbar/>
-      
+
       <div id='carte' style={{ display: "flex", flexWrap: "wrap" }}>
       {data.map((tableau, index) => (
-          <Content 
-            key={index} 
-            propriete={tableau}        
-            />
-        ))} 
+          <Content key={index} propriete={tableau}/>))} 
       </div>
+        <Panier/>
     </>
   )
 }
---
+++ src/Components/Panier/Panier.jsx
@@ -0,0 +1,13 @@
+import './Panier.css'
+
+export default function Panier (){
+
+
+
+    return(
+        <div id='last' style={{textAlign:'center'}}>
+            <div id='textPanier'>Mon panier :</div>
+            <div id='contentPanier'></div>
+        </div>
+    )
+}
